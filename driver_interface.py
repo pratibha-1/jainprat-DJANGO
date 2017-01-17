c211cb80a4fc70683a8c3894b01fa0435989f517
pratibha-1 <pratibha.2902s@gmail.com>
2017-01-17 22:45:45 +0530

    driver_interface

---
+++ driver_interface.py
@@ -0,0 +1,12 @@
+class Driver_Passenger_Matching:
+    status_of_the_ride;
+    Estimate_fare;
+
+    def matching(start_latitude,start_longitude,end_latitude,end_longitude,timestamp,Passenger_id):
+          getEstimateFare(start_latitude,start_longitude,end_latitude,end_longitude)
+          Pass driver_id/unavailability of ride to Passenger_request Api
+             
+
+
+
+
